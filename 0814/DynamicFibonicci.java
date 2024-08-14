fc5801f10722fde8080234e875499a610d5e7cc1
siddltkfkd <siddltkfkd213@gmail.com>
2024-08-14 13:23:47 +0900

    Refactor: 중복 코드 제거 및 CallStack 출력

--- 0814/DynamicFibonicci.java
+++ 0814/DynamicFibonicci.java
@@ -8,22 +8,22 @@ public class DynamicFibonicci {
     }
 
     public int fibonacci(int n) {
-        if (n == 0) {
-            fibonachiTable.put(0, 0);
-            return 0;
+        System.out.printf("Called fibonacci(%d)%n", n);
+        
+        if (n == 0 || n == 1) {
+            fibonachiTable.put(n, n);
+            System.out.printf("return fibonacci(%d) = %d%n", n, n);
+            return n;
         }
-
-        if (n == 1) {
-            fibonachiTable.put(1, 1);
-            return 1;
-        }
-
+        
         if (fibonachiTable.containsKey(n)) {
+            System.out.printf("return fibonacci(%d) = %d%n", n, fibonachiTable.get(n));
             return fibonachiTable.get(n);
         }
-
+        
         int result = fibonacci(n-1) + fibonacci(n-2);
         fibonachiTable.put(n, result);
+        System.out.printf("fibonacci(%d) = fibonacci(%d) + fibonacci(%d) = %d%n", n, n-1, n-2, result);
         
         return result;
     }
